b16dae0a397d83eb919dd25a2ac36043b0c6fe75
Israel Paz <israel_paz@msn.com>
2022-09-21 10:58:34 -0500

    correction function to get one element form json file

--- apiJson.php
+++ apiJson.php
@@ -5,10 +5,11 @@ include_once 'dbJson.php';
 class ApiJson extends DBJSON{
     
     private $jsonFile;
+    private $nameJsonFile = "json.json";
 
     public function __construct()
     {   
-        $this->jsonFile = new DBJSON('json.json');
+        $this->jsonFile = new DBJSON($this->nameJsonFile);
     }
     
     function getAll(){
@@ -17,12 +18,17 @@ class ApiJson extends DBJSON{
     
     function getOne($id){
         $jsonToArray = json_decode($this->jsonFile->getContentJson(), true);
-        $itemSearch['item'] = current(array_filter($jsonToArray['items'], function ($v) use ($id){
+        $itemToReturn = array();
+        $itemToReturn['item'] = array();
+        $fakeRow = current(array_filter($jsonToArray['items'], function ($v) use ($id){
             return $v['id'] == $id;
         }, ARRAY_FILTER_USE_BOTH));
+         
         
-        $itemSearch = json_encode($itemSearch);
-        print($itemSearch);
+        array_push($itemToReturn['item'], $fakeRow);
+
+        print(json_encode($itemToReturn));
+
     }
 
 }
